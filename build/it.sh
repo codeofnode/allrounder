e9987e9f921cbe552f130c16126336b102087507
Ramesh Kumar <codeofnode@gmail.com>
2020-04-09 12:55:02 +0530

    MASTER- fixing the build arg

--- build/it.sh
+++ build/it.sh
@@ -1,7 +1,7 @@
 
 set -e
 VER=`sed 's/.*"version": "\(.*\)".*/\1/;t;d' ../package.json`
-docker build . -t codeofnode/allrounder:$VER
+docker build .  --build-arg VERSION=${VER} -t codeofnode/allrounder:$VER
 docker tag codeofnode/allrounder:$VER codeofnode/allrounder:latest
 docker push codeofnode/allrounder:$VER
 docker push codeofnode/allrounder:latest
